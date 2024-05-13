bf93878c188ecb8c344f47e084c8603140d04e97
Karol Więckowiak <43847587+MyKarcio123@users.noreply.github.com>
2024-05-13 11:26:14 +0200

    add first iteration of generator with preview (#21)
    * add first iteration of generator with preview
    
    * fix path to assets
    
    * add relative path in cmake for all systems
    
    * cr
    
    * fix conflits

---
+++ src/Map/DungeonGenerator.cpp
@@ -0,0 +1,334 @@
+#include "DungeonGenerator.h"
+#include <algorithm>
+#include <optional>
+#include <stack>
+
+using randInt = std::uniform_int_distribution<int>;
+
+DungeonGenerator::DungeonGenerator(int height, int width) :
+    m_height(height), m_width(width), gen(std::chrono::system_clock::now().time_since_epoch().count())
+{
+}
+
+void DungeonGenerator::generateMainPath(int pathLength)
+{
+    const glm::ivec2 start{randInt(0, m_width - 1)(gen), randInt(0, m_height - 1)(gen)};
+    generateMainPath({.start{start}, .pathName{"Main"}, .pathLength{pathLength}});
+}
+
+DungeonGenerator::NodesPath DungeonGenerator::getNodes() { return m_nodeToPath; }
+
+DungeonGenerator::RoomCount DungeonGenerator::getCount() { return m_roomCount; }
+
+std::optional<char> DungeonGenerator::getLock(const glm::ivec2& node) const
+{
+    if (m_locks.contains(node)) return m_locks.at(node) + 'A';
+    return std::nullopt;
+}
+
+std::optional<char> DungeonGenerator::getKey(const glm::ivec2& node) const
+{
+    if (m_keys.contains(node)) return m_keys.at(node) + 'a';
+    return std::nullopt;
+}
+
+void DungeonGenerator::generateMainPath(const PathConfig& pathConfig)
+{
+    std::deque<glm::ivec2> path;
+    std::unordered_set<glm::ivec2> inPath;
+    std::unordered_map<glm::ivec2, std::unordered_set<glm::ivec2>> visitedNeighbors;
+
+    path.push_back(pathConfig.start);
+    inPath.insert(pathConfig.start);
+
+    while (path.size() < pathConfig.pathLength)
+    {
+        auto current{path.back()};
+        if (const auto neighbor{
+                getRandomNeighbor(current, inPath, visitedNeighbors, pathConfig.pathLength, pathConfig.pathLength + 1)})
+        {
+            const auto nextNode{neighbor.value()};
+            path.push_back(nextNode);
+            inPath.insert(nextNode);
+            visitedNeighbors[current].insert(nextNode);
+        }
+        else
+        {
+            if (current == pathConfig.start) throw std::runtime_error("Path generation failed");
+            if (constexpr auto startingNode{1}; path.size() > startingNode)
+            {
+                visitedNeighbors[current].clear();
+                path.pop_back();
+                inPath.erase(current);
+            }
+        }
+    }
+
+    int nodeIndex = 1;
+    for (const auto& pathNode : path)
+    {
+        m_pathNames.insert(pathConfig.pathName);
+        m_nodeToPath[pathNode] = pathConfig.pathName;
+        m_paths[pathConfig.pathName].insert(pathNode);
+        m_roomCount[pathNode] = nodeIndex;
+        ++nodeIndex;
+    }
+    for (int i = 0; i < path.size() - 1; i++)
+    {
+        m_graph[path[i]].insert(path[i + 1]);
+        m_uGraph[path[i]].insert(path[i + 1]);
+        m_uGraph[path[i + 1]].insert(path[i]);
+        m_nodeEnterEdgesCount[path[i + 1]]++;
+        m_nodeOutEdgesCount[path[i]]++;
+    }
+}
+
+void DungeonGenerator::generateSidePath(const sidePathConfig& pathConfig)
+{
+    validateSidePathConfig(pathConfig);
+    constexpr int startAndEndNodeOffset{2};
+    const int minPathLength{startAndEndNodeOffset + pathConfig.minPathLength};
+    const int maxPathLength{startAndEndNodeOffset + pathConfig.maxPathLength};
+
+    std::vector<glm::ivec2> pathToSidePath{m_paths[pathConfig.startingPathName].begin(),
+                                           m_paths[pathConfig.startingPathName].end()};
+
+    std::ranges::shuffle(pathToSidePath, gen);
+
+    auto start{pathToSidePath.front()};
+    glm::ivec2 end;
+
+    pathToSidePath.pop_back();
+    auto startVal{m_roomCount[start]};
+    auto endVal{std::numeric_limits<int>::max()};
+
+    std::deque<glm::ivec2> path;
+    std::unordered_set<glm::ivec2> inPath;
+    std::unordered_map<glm::ivec2, std::unordered_set<glm::ivec2>> visitedNeighbors;
+
+    path.push_back(start);
+    inPath.insert(start);
+
+    while (true)
+    {
+        auto current{path.back()};
+
+        auto neighbor{
+            getRandomNeighbor(current, inPath, visitedNeighbors, minPathLength, maxPathLength, pathConfig.endPathName)};
+        if (neighbor && path.size() <= maxPathLength)
+        {
+            const auto nextNode{neighbor.value()};
+            path.push_back(nextNode);
+            inPath.insert(nextNode);
+            visitedNeighbors[current].insert(nextNode);
+            if (m_nodeToPath[nextNode] == pathConfig.endPathName && path.size() >= minPathLength)
+            {
+                endVal = m_roomCount[nextNode];
+                end = nextNode;
+                break;
+            }
+        }
+        else
+        {
+            if (constexpr auto startingNode{1}; path.size() > startingNode)
+            {
+                visitedNeighbors[current].clear();
+                path.pop_back();
+                inPath.erase(current);
+            }
+            else
+            {
+                if (pathToSidePath.empty())
+                {
+                    throw std::runtime_error("Path generation failed, it is impossible :(");
+                }
+                visitedNeighbors[current].clear();
+                path.pop_back();
+                inPath.erase(current);
+                path.push_back(pathToSidePath.back());
+                auto startVal{m_roomCount[pathToSidePath.back()]};
+                pathToSidePath.pop_back();
+            }
+        }
+    }
+    std::vector<glm::ivec2> sidePath;
+
+    if (startVal < endVal || endVal == 0)
+    {
+        if (pathConfig.endPathName != "")
+        {
+            m_nodeEnterEdgesCount[path.back()]++;
+            path.pop_back();
+        }
+        m_nodeOutEdgesCount[path.front()]++;
+        path.pop_front();
+        sidePath = std::vector<glm::ivec2>(path.begin(), path.end());
+    }
+    else
+    {
+        if (pathConfig.endPathName != "")
+        {
+            m_nodeEnterEdgesCount[path.front()]++;
+            path.pop_front();
+        }
+        m_nodeOutEdgesCount[path.back()]++;
+        path.pop_back();
+        sidePath = std::vector<glm::ivec2>(path.rbegin(), path.rend());
+    }
+
+    m_graph[start].insert(sidePath[0]);
+    m_graph[sidePath.back()].insert(end);
+
+    m_uGraph[start].insert(sidePath[0]);
+    m_uGraph[sidePath[0]].insert(start);
+    m_uGraph[sidePath.back()].insert(end);
+    m_uGraph[end].insert(sidePath.back());
+
+    for (int i = 0; i < sidePath.size(); i++)
+    {
+        m_pathNames.insert(pathConfig.pathName);
+        m_nodeToPath[sidePath[i]] = pathConfig.pathName;
+        m_paths[pathConfig.pathName].insert(sidePath[i]);
+        m_roomCount[sidePath[i]] = i + 1;
+        m_nodeEnterEdgesCount[sidePath[i]]++;
+    }
+    for (int i = 0; i < sidePath.size() - 1; i++)
+    {
+        m_graph[sidePath[i]].insert(sidePath[i + 1]);
+        m_nodeOutEdgesCount[sidePath[i]]++;
+        m_uGraph[sidePath[i]].insert(sidePath[i + 1]);
+        m_uGraph[sidePath[i + 1]].insert(sidePath[i]);
+    }
+    if (pathConfig.endPathName != "") m_nodeOutEdgesCount[sidePath.back()]++;
+}
+
+void DungeonGenerator::makeLockAndKey()
+{
+    std::deque<std::pair<glm::ivec2, int>> nodes(m_nodeOutEdgesCount.begin(), m_nodeOutEdgesCount.end());
+
+    const auto sortByValue{[](const auto& a, const auto& b) { return a.second > b.second; }};
+
+    std::ranges::shuffle(nodes, gen);
+    std::ranges::sort(nodes, sortByValue);
+    auto a = nodes;
+    while (true)
+    {
+        const auto placeToLock{nodes.front()};
+        if (m_locks.contains(placeToLock.first) ||
+            (m_nodeToPath[placeToLock.first] == "Main" && m_roomCount[placeToLock.first] < 2) ||
+            m_roomCount[placeToLock.first] < 2)
+        {
+            nodes.pop_front();
+            continue;
+        }
+        m_locks[placeToLock.first] = m_freeKey++;
+        findPlaceForKey(placeToLock.first);
+        break;
+    }
+}
+
+void DungeonGenerator::findPlaceForKey(const glm::ivec2& lock)
+{
+    std::vector<glm::ivec2> nodesToHideKey;
+    std::deque<glm::ivec2> nodesToVisit;
+    std::unordered_set<glm::ivec2> visited{lock};
+    const auto currentPath{m_nodeToPath[lock]};
+    for (const auto& neighbor : m_uGraph[lock])
+    {
+        if (m_nodeToPath[neighbor] != currentPath) continue;
+        if (m_roomCount[neighbor] >= m_roomCount[lock]) continue;
+        nodesToHideKey.push_back(neighbor);
+        nodesToVisit.push_back(neighbor);
+        visited.insert(neighbor);
+    }
+    while (!nodesToVisit.empty())
+    {
+        const auto current{nodesToVisit.front()};
+        nodesToVisit.pop_front();
+        for (const auto& neighbor : m_uGraph[current])
+        {
+            if (visited.contains(neighbor)) continue;
+            nodesToHideKey.push_back(neighbor);
+            nodesToVisit.push_back(neighbor);
+            visited.insert(neighbor);
+        }
+    }
+    std::ranges::shuffle(nodesToHideKey, gen);
+    m_keys[nodesToHideKey.front()] = m_locks[lock];
+    // TODO: priorytezacja kluczy w dead endach labiryntu
+}
+
+std::optional<glm::ivec2> DungeonGenerator::getRandomNeighbor(
+    const glm::ivec2& current, const std::unordered_set<glm::ivec2>& nodesInPath,
+    const std::unordered_map<glm::ivec2, std::unordered_set<glm::ivec2>>& visitedNeighbors, int minL, int maxL,
+    const std::string& endPath)
+{
+    std::vector<glm::ivec2> neighbors;
+    constexpr glm::ivec2 directions[]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    const auto checkIfNeighborIsCorrect{
+        [this, &current, &nodesInPath, &visitedNeighbors, &neighbors, &endPath, &maxL, &minL](const auto& direction)
+        {
+            const auto neighbor{current + direction};
+            const auto insideGraph{neighbor.x >= 0 && neighbor.x < m_width && neighbor.y >= 0 && neighbor.y < m_height};
+            const auto isNotInOtherPath{m_nodeToPath[neighbor] == "" || m_nodeToPath[neighbor] == endPath};
+            if (insideGraph && isNotInOtherPath)
+            {
+                const auto nodeInPath{nodesInPath.contains(neighbor)};
+                const auto nodeVisited{visitedNeighbors.contains(current) &&
+                                       visitedNeighbors.at(current).contains(neighbor)};
+                const auto correctPathLength{nodesInPath.size() < maxL};
+                if (!nodeVisited && !nodeInPath && correctPathLength)
+                {
+                    if (!endPath.empty() && m_nodeToPath[neighbor] == endPath && nodesInPath.size() < minL)
+                    {
+                        return;
+                    }
+                    neighbors.push_back(neighbor);
+                }
+            }
+        }};
+    std::ranges::for_each(directions, checkIfNeighborIsCorrect);
+
+    if (!neighbors.empty())
+    {
+        const auto nextNodeIndex{randInt(0, static_cast<int>(std::size(neighbors)) - 1)(gen)};
+        return neighbors.at(nextNodeIndex);
+    }
+
+    return std::nullopt;
+}
+
+void DungeonGenerator::validateSidePathConfig(const sidePathConfig& pathConfig) const
+{
+    if (m_pathNames.contains(pathConfig.pathName))
+    {
+        throw std::logic_error("Path with this name already exists");
+    }
+    if (pathConfig.maxPathLength < pathConfig.minPathLength)
+    {
+        throw std::logic_error("Max path length must be greater than min path length");
+    }
+    if (pathConfig.minPathLength < 1)
+    {
+        throw std::logic_error("Path length must be greater than 0");
+    }
+    if (!m_pathNames.contains(pathConfig.startingPathName))
+    {
+        throw std::logic_error("Side path should start somewhere");
+    }
+}
+void DungeonGenerator::validateMainPath(const PathConfig& pathConfig) const
+{
+    if (m_pathNames.contains(pathConfig.pathName))
+    {
+        throw std::logic_error("Path with this name already exists");
+    }
+    if (pathConfig.pathLength < 1)
+    {
+        throw std::logic_error("Path length must be greater than 0");
+    }
+    if (pathConfig.pathLength > m_width * m_height)
+    {
+        throw std::logic_error("Path length is too long");
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#define GLM_ENABLE_EXPERIMENTAL
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 #include <imgui-SFML.h>
 #include <imgui.h>
+#include <unordered_map>
 
 #include "Coordinator.h"
+#include "DungeonGenerator.h"
 #include "InputHandler.h"
 #include "Paths.h"
 #include "PlayerComponent.h"
@@ -15,6 +18,92 @@
 
 Coordinator gCoordinator;
 
+void previevGenerator(sf::Font& font, std::vector<sf::RectangleShape>& rectangles, std::vector<sf::Text>& texts)
+{
+    /*Map Generator Preview Start*/
+    int h = 5;
+    int w = 6;
+    DungeonGenerator generator(h, w);
+    generator.generateMainPath(11);
+    generator.generateSidePath(
+        {.pathName{"FirstC"}, .startingPathName{"Main"}, .endPathName{"Main"}, .minPathLength{3}, .maxPathLength{5}});
+    generator.generateSidePath(
+        {.pathName{"SecondC"}, .startingPathName{"Main"}, .endPathName{""}, .minPathLength{3}, .maxPathLength{5}});
+    const int rectWidth = 50;
+    const int rectHeight = 50;
+    const int gap = 20;
+
+
+    auto fullMap = generator.getNodes();
+    auto roomCount = generator.getCount();
+    generator.makeLockAndKey();
+    for (int i = 0; i < w; ++i)
+    {
+        for (int j = 0; j < h; ++j)
+        {
+            sf::RectangleShape rectangle(sf::Vector2f(rectWidth, rectHeight));
+            rectangle.setPosition(i * (rectWidth + gap), j * (rectHeight + gap));
+
+            if (fullMap[{i, j}] == "Main")
+            {
+                rectangle.setFillColor(sf::Color::Red);
+            }
+            else if (fullMap[{i, j}] == "FirstC")
+            {
+                rectangle.setFillColor(sf::Color::Blue);
+            }
+            else if (fullMap[{i, j}] == "SecondC")
+            {
+                rectangle.setFillColor(sf::Color::Green);
+            }
+            else
+            {
+                rectangle.setFillColor(sf::Color::Black);
+            }
+            rectangles.push_back(rectangle);
+
+            auto currentNodeVal = roomCount[{i, j}];
+            sf::Text text;
+            text.setFont(font);
+            text.setString(std::to_string(currentNodeVal));
+            text.setCharacterSize(14); // w pikselach
+            text.setFillColor(sf::Color::White);
+            text.setPosition(i * (rectWidth + gap), j * (rectHeight + gap));
+
+            texts.push_back(text);
+
+
+            sf::Text key;
+            key.setFont(font);
+            key.setString(std::to_string(generator.m_nodeOutEdgesCount[{i, j}]));
+            key.setCharacterSize(14); // w pikselach
+            key.setFillColor(sf::Color::Black);
+            key.setPosition(i * (rectWidth + gap) + 30, j * (rectHeight + gap) + 30);
+            texts.push_back(key);
+            if (const auto text = generator.getKey({i, j}))
+            {
+                sf::Text key;
+                key.setFont(font);
+                key.setString(text.value());
+                key.setCharacterSize(14); // w pikselach
+                key.setFillColor(sf::Color::Black);
+                key.setPosition(i * (rectWidth + gap) + 10, j * (rectHeight + gap) + 10);
+                texts.push_back(key);
+            }
+            if (const auto text = generator.getLock({i, j}))
+            {
+                sf::Text key;
+                key.setFont(font);
+                key.setString(text.value());
+                key.setCharacterSize(14); // w pikselach
+                key.setFillColor(sf::Color::Black);
+                key.setPosition(i * (rectWidth + gap) + 10, j * (rectHeight + gap) + 10);
+                texts.push_back(key);
+            }
+        }
+    }
+    /*Map Generator Preview End*/
+}
 int main()
 {
     gCoordinator.init();
@@ -57,51 +146,69 @@ int main()
     window.setFramerateLimit(60);
     auto _ = ImGui::SFML::Init(window);
 
-    sf::CircleShape shape(100.f);
-
     sf::Clock deltaClock;
     while (window.isOpen())
     {
         sf::Event event{};
         InputHandler::getInstance()->update();
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(event);
 
-            if (event.type == sf::Event::KeyPressed)
+        sf::Font font;
+        const std::string assetPath = ASSET_PATH;
+        font.loadFromFile(assetPath + "/fonts/Bentinck-Regular.ttf");
+
+        std::vector<sf::RectangleShape> rectangles;
+        std::vector<sf::Text> texts;
+
+        previevGenerator(font, rectangles, texts);
+
+        while (window.isOpen())
+        {
+            sf::Event event{};
+            while (window.pollEvent(event))
             {
-                const auto keyCode = event.key.code;
-                InputHandler::getInstance()->handleKeyboardInput(keyCode, true);
+                ImGui::SFML::ProcessEvent(event);
+
+                if (event.type == sf::Event::KeyPressed)
+                {
+                    const auto keyCode = event.key.code;
+                    InputHandler::getInstance()->handleKeyboardInput(keyCode, true);
+                }
+                else if (event.type == sf::Event::KeyReleased)
+                {
+                    const auto keyCode = event.key.code;
+                    InputHandler::getInstance()->handleKeyboardInput(keyCode, false);
+                }
+                if (event.type == sf::Event::Closed)
+                {
+                    window.close();
+                }
             }
-            else if (event.type == sf::Event::KeyReleased)
-            {
-                const auto keyCode = event.key.code;
-                InputHandler::getInstance()->handleKeyboardInput(keyCode, false);
-            }
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+
+            playerMovementSystem->update();
+
+
+            ImGui::SFML::Update(window, deltaClock.restart());
+
+            ImGui::Begin("Hello, world!");
+            ImGui::Button("Look at this pretty button");
+            ImGui::End();
+
+            ImGui::ShowDemoWindow();
+
+            window.clear();
+
+            /*Draw generator start*/
+            for (const auto& rectangle : rectangles) window.draw(rectangle);
+            for (const auto& text : texts) window.draw(text);
+            /*Draw generator end*/
+
+            renderSystem->draw(window);
+            ImGui::SFML::Render(window);
+            window.display();
         }
 
-        playerMovementSystem->update();
+        ImGui::SFML::Shutdown();
 
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        ImGui::ShowDemoWindow();
-
-        window.clear();
-        renderSystem->draw(window);
-        ImGui::SFML::Render(window);
-        window.display();
+        return 0;
     }
-
-    ImGui::SFML::Shutdown();
-
-    return 0;
 }
