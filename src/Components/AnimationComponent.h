ad883f75c0a52bfa214a8a62388a06e40869a0b8
Karol Więckowiak <43847587+MyKarcio123@users.noreply.github.com>
2024-09-21 12:14:26 +0200

    delete iterator with unpredictable behaviour (#65)
    * delete iterator with unpredictable behaviour
    
    * clear code

--- src/Components/AnimationComponent.h
+++ src/Components/AnimationComponent.h
@@ -1,20 +1,12 @@
 #pragma once
 
-#include <utility>
 #include <vector>
 #include "AnimationFrame.h"
-#include "AtlasComponents/Texture.h"
-#include "CircularIterator.h"
 
 struct AnimationComponent
 {
     std::vector<AnimationFrame> frames{};
-    CircularIterator<std::vector<AnimationFrame>::iterator> it;
-    [[maybe_unused]] bool loop_animation = true;
+    size_t currentFrame{};
 
-    AnimationComponent() : it(frames.begin(), frames.end()) {}
-    explicit AnimationComponent(const std::vector<AnimationFrame>& frames) :
-        frames(frames), it(this->frames.begin(), this->frames.end())
-    {
-    }
+    [[maybe_unused]] bool loop_animation{true};
 };
--- src/Utils/CircularIterator.h
+++ src/Utils/CircularIterator.h
@@ -1,34 +1,49 @@
 #pragma once
 
-template<typename Iterator>
-class CircularIterator {
+//Deprecated makes random error on VS22 in windows 10
+
+#include <cassert>
+
+template <typename Iterator>
+class CircularIterator
+{
 public:
     CircularIterator(Iterator begin, Iterator end)
-        : begin_(begin), end_(end), current_(begin) {}
+        : begin_(begin), end_(end), current_(begin)
+    {
+        assert(begin_ != end_ && "CircularIterator cannot be initialized with an empty range");
+    }
 
-    typename Iterator::value_type& operator*() {
+    typename Iterator::value_type& operator*()
+    {
+        assert(current_ != end_ && "Cannot dereference end iterator");
         return *current_;
     }
 
-    CircularIterator& operator++() {
+    CircularIterator& operator++()
+    {
+        assert(current_ != end_ && "Cannot increment past the end of the range");
         ++current_;
-        if (current_ == end_) {
+        if (current_ == end_)
             current_ = begin_;
-        }
         return *this;
     }
 
-    CircularIterator operator++(int) {
+    CircularIterator operator++(int)
+    {
         CircularIterator temp = *this;
         ++(*this);
         return temp;
     }
 
-    bool operator!=(const CircularIterator& other) const {
+    bool operator!=(const CircularIterator& other) const
+    {
         return current_ != other.current_;
     }
 
-    typename Iterator::value_type* operator->() {
+    typename Iterator::value_type* operator->()
+    {
+        assert(current_ != end_ && "Cannot access element through end iterator");
         return &(*current_);
     }
 
