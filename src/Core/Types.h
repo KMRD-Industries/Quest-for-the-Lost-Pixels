07c68733964de5e66689676b3b317f43b9634ed8
dominiks01 <dominikszot950@gmail.com>
2024-10-23 10:30:17 +0200

    Testing new map

--- src/Core/Types.h
+++ src/Core/Types.h
@@ -6,7 +6,7 @@
 using Entity = std::uint32_t;
 using ComponentType = std::uint8_t;
 
-constexpr Entity MAX_ENTITIES = 5000;
+constexpr Entity MAX_ENTITIES = 500000;
 constexpr ComponentType MAX_COMPONENTS = 32;
 
 using Signature = std::bitset<MAX_COMPONENTS>;
--- src/Map/Dungeon.cpp
+++ src/Map/Dungeon.cpp
@@ -120,17 +120,11 @@ void Dungeon::render(sf::RenderWindow& window)
 
 void Dungeon::addPlayerComponents(const Entity player)
 {
-    gCoordinator.addComponents(player,
-        TileComponent{configSingleton.GetConfig().playerAnimation, "Characters", 5},
-        RenderComponent{},
-        TransformComponent{GameUtility::startingPosition},
-        AnimationComponent{},
-        CharacterComponent{.hp = configSingleton.GetConfig().defaultCharacterHP},
-        PlayerComponent{},
-        ColliderComponent{},
-        InventoryComponent{},
-        EquipmentComponent{},
-        FloorComponent{},
+    gCoordinator.addComponents(
+        player, TileComponent{configSingleton.GetConfig().playerAnimation, "Characters", 5}, RenderComponent{},
+        TransformComponent{GameUtility::startingPosition}, AnimationComponent{},
+        CharacterComponent{.hp = configSingleton.GetConfig().defaultCharacterHP}, PlayerComponent{},
+        ColliderComponent{}, InventoryComponent{}, EquipmentComponent{}, FloorComponent{},
         TravellingDungeonComponent{.moveCallback = [this](const glm::ivec2& dir) { moveInDungeon(dir); }},
         PassageComponent{.moveCallback = [this] { moveDownDungeon(); }});
 }
@@ -140,16 +134,14 @@ void Dungeon::createRemotePlayer(const uint32_t id)
     const auto tag = std::format("Player {}", id);
     m_entities[id] = gCoordinator.createEntity();
 
-    gCoordinator.addComponents(m_entities[id],
+    gCoordinator.addComponents(
+        m_entities[id],
         TransformComponent(sf::Vector2f(getSpawnOffset(configSingleton.GetConfig().startingPosition.x, id),
                                         getSpawnOffset(configSingleton.GetConfig().startingPosition.y, id)),
                            0.f, sf::Vector2f(1.f, 1.f), {0.f, 0.f}),
-        TileComponent{configSingleton.GetConfig().playerAnimation, "Characters", 3},
-        RenderComponent{},
-        AnimationComponent{},
-        CharacterComponent{.hp = configSingleton.GetConfig().defaultCharacterHP},
-        MultiplayerComponent{},
-        ColliderComponent{});
+        TileComponent{configSingleton.GetConfig().playerAnimation, "Characters", 3}, RenderComponent{},
+        AnimationComponent{}, CharacterComponent{.hp = configSingleton.GetConfig().defaultCharacterHP},
+        MultiplayerComponent{}, ColliderComponent{});
 
     Collision cc = gCoordinator.getRegisterSystem<TextureSystem>()->getCollision(
         "Characters", configSingleton.GetConfig().playerAnimation);
@@ -317,7 +309,7 @@ void Dungeon::changeRoom(const glm::ivec2& room)
 
 void Dungeon::makeStartFloor()
 {
-    Room room{"0", 0};
+    Room room{"17", 1};
     m_roomMap.clear();
     m_roomMap.emplace(glm::ivec2{0, 0}, room);
     m_currentPlayerPos = {0.f, 0.f};
